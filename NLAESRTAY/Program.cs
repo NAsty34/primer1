5685aebb2f9b4edcc0839dac61f0a704ebad8254
NAsty34 <99412848+NAsty34@users.noreply.github.com>
2022-03-01 11:27:22 +0300

    Add files via upload

---
+++ NLAESRTAY/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NLAESRTAY
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // массив входных обучающих векторов
+            Vector[] X = {
+    new Vector(0, 0),
+    new Vector(0, 1),
+    new Vector(1, 0),
+    new Vector(1, 1)
+};
+
+            // массив выходных обучающих векторов
+            Vector[] Y = {
+    new Vector(0.0), // 0 ^ 0 = 0
+    new Vector(1.0), // 0 ^ 1 = 1
+    new Vector(1.0), // 1 ^ 0 = 1
+    new Vector(0.0) // 1 ^ 1 = 0
+};
+
+            Network network = new Network(new int[] { 2, 3, 1 }); // создаём сеть с двумя входами, тремя нейронами в скрытом слое и одним выходом
+            network.Train(X, Y, 0.5, 1e-7, 100000); // запускаем обучение сети 
+            for (int i = 0; i < 4; i++)
+            {
+                Vector output = network.Forward(X[i]);
+                Console.WriteLine("X: {0} {1}, Y: {2}, output: {3}", X[i][0], X[i][1], Y[i][0], output[0]);
+            }
+        }
+    }
+   
+
+    
+}
+
+
